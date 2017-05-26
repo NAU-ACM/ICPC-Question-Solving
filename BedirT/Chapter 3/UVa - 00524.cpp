352ddee7ec592733e9ea2f8f984db5a74bf7bf5b
BedirT <mtapkan@na.edu>
2017-05-26 10:20:11 -0500

    524 updated

--- BedirT/Chapter 3/UVa - 00524.cpp
+++ BedirT/Chapter 3/UVa - 00524.cpp
@@ -2,58 +2,57 @@
 	Question: UVa - 00524
 	Related Topic: Backtrack
 	Author: BedirT
-	Date: 2016 ...
+	Date: 05/25/2017
 */
-#include <cstdio>
+#include <iostream>
 #include <bitset>
 #include <vector>
+#include <fstream>
+// #define debug
 using namespace std;
-int primes[11] = {2,3,5,7,11,13,17,19,23,29,31}; 	// max=15+16
+int primes[11] = {2,3,5,7,11,13,17,19,23,29,31}, n, cycle[20];
 bitset<20> used;
-int cycle[20];
-int n;
 
 bool isPrime (int num) {
-	for (int i=0; i<11; ++i)
-		if (num == primes[i]) return true;
+	for(int i = 0; i < 11; ++i)
+		if(num == primes[i]) return true;
 	return false;
 }
 
-bool place (int pos, int x) {
-	if (pos != n-1) {
-		if(isPrime(cycle[pos-1]+x))return true;
-		else return false;
-	}
-	else {
-		if(isPrime(cycle[pos-1]+x) && isPrime(x+cycle[0])) return true;
-		else return false;
-	}
+bool beans (int pos, int x) {
+	if(pos == n-1 && isPrime(x + cycle[0]) && isPrime(x + cycle[pos-1])) return true; 
+	else if(pos != n-1 && isPrime(x + cycle[pos-1])) return true;
+	else return false;
 }
 
-void backtrack (int k) {
-	if (k == n) {
-		for (int i=0; i < n; ++i)
-			printf ("%d ", cycle[i]);
-		printf("\n");
-		return;
+void backtrack (int x) {
+	if(x == n){
+		for(int i = 0; i < n; ++i){
+			cout << cycle[i] << (i == n-1 ? "\n" : " ");
+		} return;
 	}
-	for (int i=2; i <= n; ++i) {
-		if (!used[i] && place(k,i)) {
-			cycle[k] = i;
-			used[i] = true;
-			backtrack(k+1);
-			used[i] = false; // We are done, so resetting
+	for(int i = 2; i <= n; ++i){
+		if(!used[i] && beans(x, i)){
+			used[i] = 1;
+			cycle[x] = i;
+			backtrack(x+1);
+			used[i] = 0;
 		}
 	}
 }
 
 int main () {
 	int t = 1;
-	while (cin >> n){
-		printf("Case %d\n", t++);
-		cycle[0] = 1;
-		used[0] = true;
+	#ifdef debug
+		freopen("output.txt", "w", stdout);
+	#endif
+	while(cin >> n){
+		if(t != 1) cout << endl;
+		printf("Case %d:\n", t++);
+		used[0] = cycle[0] = 1;
 		backtrack(1);
-		printf("\n");
 	}
+	#ifdef debug
+		fclose(stdout);
+	#endif
 }
