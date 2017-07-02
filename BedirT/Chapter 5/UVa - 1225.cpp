60259d300dd4f4ce7f06b554b2947ed8a247ea5e
BedirT <mtapkan@na.edu>
2017-07-02 12:04:01 -0500

    1225 added

---
+++ BedirT/Chapter 5/UVa - 1225.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+int dig[10];
+
+void addDigits(int x){
+	while(x > 0){
+		dig[x%10]++;
+		x /= 10;
+	}
+}
+
+int main (){
+	int t, n;
+	// freopen("out.txt", "w", stdout);
+	cin >> t;
+	while(t--){
+		memset(dig, 0, sizeof dig);
+		cin >> n;
+		for(int i = 1; i <= n; ++i)
+			addDigits(i);
+		for(int i = 0; i < 10; ++i)
+			cout << dig[i] << (i == 9 ? "\n" : " ");
+	}
+	// fclose(stdout);
+}
